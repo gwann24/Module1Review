48a7a19f2001753389de2a44dfc296b10bf86cd1
Greg Wanner <greg@tegogroup.com>
2024-02-19 14:49:57 -0500

    Step 2, added area of triangle calc method

--- Module1Review/Program.cs
+++ Module1Review/Program.cs
@@ -7,6 +7,11 @@
         {
             return Math.PI * Math.Pow(radius, 2);
         }
+        // Method to calculate area of a triangle based on base length and height paramater.
+        public static double AreaOfTriangle(double baselength, double height)
+        {
+            return (baselength * height)/2;
+        }
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
