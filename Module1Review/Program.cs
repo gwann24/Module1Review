3d3dd6455a4f3339402f2e0e49b4051851969e46
Greg Wanner <greg@tegogroup.com>
2024-02-19 14:42:13 -0500

    Step 2, added area of circle calc method

---
+++ Module1Review/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Module1Review
+{
+    public class Program
+    {
+        // Method to calculate area of a circle based on radius paramater.
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
